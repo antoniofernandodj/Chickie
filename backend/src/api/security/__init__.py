a26db5aa0cf20d869a94eac1d57542f3a10c009e
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-19 03:20:05 -0300

    security

--- backend/src/api/security/__init__.py
+++ backend/src/api/security/__init__.py
@@ -2,15 +2,8 @@ from src.api.security.company import current_company, authenticate_company
 from src.api.security.user import current_user, authenticate_user
 from src.api.security.scheme import oauth2_scheme
 from src.api.security.token import create_access_token
+from src.api.security.auth_service import AuthService  # noqa
 import base64
 import bcrypt
 
-
-class HashService:
-
-    @classmethod
-    def hash(cls, password: str):
-        salt = bcrypt.gensalt()
-        hashpw = bcrypt.hashpw(password.encode("utf-8"), salt)
-        hash_base64 = base64.b64encode(hashpw).decode("utf-8")
-        return hash_base64
+from src.api.security.hash_service import HashService  # noqa
