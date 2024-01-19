67181f941207db855c7f50958c8b0983a9003d3f
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-19 03:23:20 -0300

    feat: console colors

--- backend/src/misc/__init__.py
+++ backend/src/misc/__init__.py
@@ -1,5 +1,6 @@
 import tomli
 from typing import TypedDict, List
+from .paginate import Paginador  # noqa
 
 
 class Info(TypedDict):
@@ -26,3 +27,42 @@ def get_project_info() -> Info:
         info['authors'] = toml_data["tool"]["poetry"]["authors"]
 
     return info
+
+
+class ConsoleColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'  # fim da cor
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    @classmethod
+    def header(cls, string: str) -> str:
+        return cls.HEADER + string + cls.ENDC
+
+    @classmethod
+    def okblue(cls, string: str) -> str:
+        return cls.OKBLUE + string + cls.ENDC
+
+    @classmethod
+    def okgreen(cls, string: str) -> str:
+        return cls.OKGREEN + string + cls.ENDC
+
+    @classmethod
+    def warning(cls, string: str) -> str:
+        return cls.WARNING + string + cls.ENDC
+
+    @classmethod
+    def fail(cls, string: str) -> str:
+        return cls.FAIL + string + cls.ENDC
+
+    @classmethod
+    def bold(cls, string: str) -> str:
+        return cls.BOLD + string + cls.ENDC
+
+    @classmethod
+    def underline(cls, string: str) -> str:
+        return cls.UNDERLINE + string + cls.ENDC
