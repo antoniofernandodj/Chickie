558252403319cff12f0fe49bf42fb9e540523743
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-19 03:34:40 -0300

    rm unused items

--- backend/src/api/security/__init__.py
+++ backend/src/api/security/__init__.py
@@ -1,9 +1,8 @@
-from src.api.security.company import current_company, authenticate_company
-from src.api.security.user import current_user, authenticate_user
-from src.api.security.scheme import oauth2_scheme
-from src.api.security.token import create_access_token
+from src.api.security.company import current_company, authenticate_company  # noqa
+from src.api.security.user import current_user, authenticate_user  # noqa
+from src.api.security.scheme import oauth2_scheme  # noqa
+from src.api.security.token import create_access_token  # noqa
 from src.api.security.auth_service import AuthService  # noqa
-import base64
-import bcrypt
+
 
 from src.api.security.hash_service import HashService  # noqa
