e07adb2dfe3d773ba3f6ac728c491801dc2116d9
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-19 03:31:56 -0300

    rm unused components

--- backend/src/infra/database_postgres/config.py
+++ backend/src/infra/database_postgres/config.py
@@ -1,32 +1,12 @@
 from time import sleep
 from config import settings as s
 from sqlalchemy.engine import create_engine
-from src.infra.database_postgres.manager import DatabaseConnectionManager
 from sqlalchemy.orm.session import sessionmaker
 
-
-# database_url = "{0}+{1}://{2}:{3}@{4}/{5}".format(
-#     "postgresql",
-#     "psycopg2",
-#     s.POSTGRES_USERNAME,
-#     s.POSTGRES_PASSWORD,
-#     s.POSTGRES_HOST,
-#     s.POSTGRES_DATABASE,
-# )
 
 database_url = s.POSTGRES_URL
 
 engine = create_engine(database_url)
-
-
-async def init_database(database_name: str):
-    from src.infra.database_postgres.entities import Base
-
-    # await DatabaseConnectionManager.create_database(
-    #     name=s.POSTGRES_DATABASE_PROD
-    # )
-    await DatabaseConnectionManager.create_database(name=database_name)
-    Base.metadata.create_all(engine)
 
 
 def get_session():
