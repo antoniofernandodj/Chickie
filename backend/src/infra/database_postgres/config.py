870b2bf602de7fb7bf53cb24228f4660b7ce2041
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-19 03:39:57 -0300

    rm unused items

--- backend/src/infra/database_postgres/config.py
+++ backend/src/infra/database_postgres/config.py
@@ -1,26 +1,5 @@
-from time import sleep
 from config import settings as s
 from sqlalchemy.engine import create_engine
-from sqlalchemy.orm.session import sessionmaker
 
 
-database_url = s.POSTGRES_URL
-
-engine = create_engine(database_url)
-
-
-def get_session():
-    session = None
-    trying = True
-    while trying:
-        try:
-            Session = sessionmaker(engine)
-            session = Session()
-            yield session
-            trying = False
-        except Exception:
-            print('Waiting db...')
-            sleep(5)
-
-    if session:
-        session.close()
+engine = create_engine(str(s.POSTGRES_URL))
--- backend/src/infra/database_postgres/interfaces/__init__.py
+++
@@ -1,1 +0,0 @@
-from .interfaces import CommandInterface  # noqa
