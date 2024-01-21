b762dcfdd4df6e0399ed06f0aab515008383206f
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-21 16:59:55 -0300

    tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,11 @@
-from config import settings as s
+import sys
+import os
+from pathlib import Path
+parent = str(Path(os.path.dirname(__file__)).parent)
+
+sys.path.append(parent)
+
+# from config import settings as s
 from fastapi.testclient import TestClient  # noqa
 from src.domain.models import Loja  # noqa
 import json  # noqa
@@ -7,30 +14,30 @@ import sys  # noqa
 from src import create_app  # noqa
 
 
-@pytest.fixture
-def client():
-    app = create_app(sys.argv)
-    client = TestClient(app)
-    return client
+# @pytest.fixture
+# def client():
+#     app = create_app(sys.argv)
+#     client = TestClient(app)
+#     return client
 
 
-@pytest.fixture
-def loja_uuid(client: TestClient):
-    data = {"username": s.LOJA_USERNAME, "password": s.LOJA_SENHA}
-    response = client.post("/loja/login", data=data)
-    return json.loads(response.text)["uuid"]
+# @pytest.fixture
+# def loja_uuid(client: TestClient):
+#     data = {"username": s.LOJA_USERNAME, "password": s.LOJA_SENHA}
+#     response = client.post("/loja/login", data=data)
+#     return json.loads(response.text)["uuid"]
 
 
-@pytest.fixture
-def loja(loja_uuid: str, client: TestClient):
-    response = client.get(f"/loja/{loja_uuid}")
-    return Loja(**json.loads(response.text))
+# @pytest.fixture
+# def loja(loja_uuid: str, client: TestClient):
+#     response = client.get(f"/loja/{loja_uuid}")
+#     return Loja(**json.loads(response.text))
 
 
-@pytest.fixture
-def access_token(client: TestClient):
-    response = client.post(
-        "/loja/login",
-        data={"username": s.LOJA_USERNAME, "password": s.LOJA_SENHA},
-    )
-    return json.loads(response.text)["access_token"]
+# @pytest.fixture
+# def access_token(client: TestClient):
+#     response = client.post(
+#         "/loja/login",
+#         data={"username": s.LOJA_USERNAME, "password": s.LOJA_SENHA},
+#     )
+#     return json.loads(response.text)["access_token"]
---
+++ backend/tests/test_command_handler.py
@@ -0,0 +1,42 @@
+from src.infra.database_postgres import DSN
+from src.infra.database_postgres.repository import CommandHandler
+from src.domain.models import CategoriaProdutos
+import aiopg
+import psycopg2
+import pytest
+
+
+LOJA_UUID = '2196ed4a-a43c-4ff6-aecd-56da06e03004'
+
+
+async def test_command_handler():
+    async with aiopg.create_pool(DSN) as pool:
+        async with pool.acquire() as connection:
+
+            handler = CommandHandler(CategoriaProdutos, connection)
+
+            categoria1 = CategoriaProdutos(
+                nome='cat1', descricao='desc',
+                loja_uuid=LOJA_UUID
+            )
+
+            categoria2 = CategoriaProdutos(
+                nome='cat2', descricao='desc',
+                loja_uuid=LOJA_UUID
+            )
+
+            categoria3 = CategoriaProdutos(
+                nome='cat3', descricao='desc',
+                loja_uuid=''  # raises HERE!
+            )
+
+            handler.save([categoria1, categoria2, categoria3])
+
+            assert len(handler.commands) == 3
+
+            results = []
+            with pytest.raises(psycopg2.errors.ForeignKeyViolation):
+                result = await handler.commit()
+                results.extend(result)
+
+            assert len(results) == 0
