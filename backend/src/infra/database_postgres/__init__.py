a233c488b94903dff6e6de0c8cc725c5d07a862c
Antonio Fernando <antoniofernandodj@outlook.com>
2024-01-21 17:01:14 -0300

    dsn

--- backend/src/infra/database_postgres/__init__.py
+++ backend/src/infra/database_postgres/__init__.py
@@ -4,4 +4,13 @@ PostgreSQL, configurar sua classe de repositório e todas as dependencias
 e modulos relacionados
 """
 
+from config import settings
+
+DSN = ("dbname={0} user={1} password={2} host={3}".format(
+    settings.POSTGRES_DATABASE,
+    settings.POSTGRES_USERNAME,
+    settings.POSTGRES_PASSWORD,
+    settings.POSTGRES_HOST,
+))
+
 from . import config, session, entities  # noqa
